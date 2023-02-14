91a4302b86453b229220baaa81aef2cac069d7b0
Kevin Okoth <kevinokotth04@gmail.com>
2023-02-14 16:11:05 +0300

    Update markup

---
+++ js/Index.js
@@ -0,0 +1,11 @@
+// create an array for book collection
+// function to add book to collection
+// function to remove book from collection
+const bookCollection = [];
+
+const title = document.getElementById('book-title');
+const author = document.getElementById('book-author');
+const btnAdd = document.getElementById('add');
+
+//display boooks
+const 
